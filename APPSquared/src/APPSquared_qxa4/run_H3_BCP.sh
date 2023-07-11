897a1f5e97a132f8112f7f83b975a08dcc6c2a98
Nicole Paterson <83827737+nicolepaterson@users.noreply.github.com>
2023-07-11 13:11:19 -0400

    Update run_H3_BCP.sh

--- APPSquared/src/APPSquared_qxa4/run_H3_BCP.sh
+++ APPSquared/src/APPSquared_qxa4/run_H3_BCP.sh
@@ -4,7 +4,7 @@
 
 
 usage() { echo "Usage: $0 -r H3 reference structure for rmsd in .mae format, -d output directory, -n name of new output directory "; exit 1; }
-while getopts ":r:d:n:" arg; 
+while getopts ":r:d:n:" arg;
 do
         case "${arg}" in
                 r)
@@ -33,74 +33,76 @@ BCPhome=/scicomp/groups/OID/NCIRD/ID/VSDB/GAT
 
 #ml conda
 #make the directory for storing the data
-mkdir $name
-#cd $directory/$name
+mkdir $directory/$name
 
 #Runs AlphaFold on sequence fasta file
 #run Rosetta energy score for ddG
 
 module load rosetta/3.13-cluster
 if [ -e "-o" ]; then
-#bash run_rosetta_scores.sh -d $reference -n $name
+    bash run_rosetta_scores.sh -d $reference -n $name
 #for file in $directory/prep*.pdb
-    bash run_rosetta_scores.sh -d $directory -n $name
+#bash run_rosetta_scores.sh -d $directory -n $name
 fi
 #Computes the glycosylation distances for each site from pdb file
 ml conda
-conda activate glyc
-bash run_glyc.sh -i $directory
-bash run_glyc_diff.sh -i $directory -r $reference
+    conda activate glyc
+    bash run_glyc.sh -i $directory
+#bash run_glyc_diff.sh -i $directory -r $reference
 #python glyc_score.py -i $directory -r $reference
 
+ml conda
 conda activate getcontacts
 for file in $directory/prep*.pdb
 do
     filename=$(basename "$file")
     echo $filename
     #sudo /home/qxa4/schrodinger/utilities/prepwizard $filename prep_$filename
-    python get_static_contacts.py --structure $filename --output $directory/$name/$filename.H3static_contacts.csv --itypes all
-    python get_contact_frequencies.py --input_files $directory/$name/$filename.H3static_contacts.csv --output_file $directory/$name/$filename.H3freq.tsv --itypes all
+    python get_static_contacts.py --structure $filename --output $name/$filename.H3static_contacts.csv --itypes all
     sleep 5
-    python contact_diffs.py -i $directory/$name/$filename.H3static_contacts.csv -r $reference.static_contacts.csv -o $directory/$name/$filename.H3contactdiffs.static.csv
+    python get_contact_frequencies.py --input_files $name/$filename.H3static_contacts.csv --output_file $name/$filename.H3freq.tsv --itypes all
     sleep 5
-    python get_contact_flare.py --input $directory/$name/$filename.H3static_contacts.csv --out $directory/$name/$filename.H3contact_flare.json
-    python H3_rbs_check.py -i $directory/$name/$filename.H3contactdiffs.static.csv -o $directory/$name/$filename.H3rbs.csv
-    python sin_check.py -i $directory/$name/$filename.H3contactdiffs.static.csv -o $directory/$name/$filename.H3_SIN.csv
-    python siteA_check.py -i $directory/$name/$filename.H3contactdiffs.static.csv -o $directory/$name/$filename.H3siteA.csv
-    python siteB_check.py -i $directory/$name/$filename.H3contactdiffs.static.csv -o $directory/$name/$filename.H3siteB.csv
-    python siteC_check.py -i $directory/$name/$filename.H3contactdiffs.static.csv -o $directory/$name/$filename.H3siteC.csv
-    python siteD_check.py -i $directory/$name/$filename.H3contactdiffs.static.csv -o $directory/$name/$filename.H3siteD.csv
-    python siteE_check.py -i $directory/$name/$filename.H3contactdiffs.static.csv -o $directory/$name/$filename.H3siteE.csv
-    python H3_rbs_check.py -i $directory/$name/$filename.H3siteA.csv -o $directory/$name/$filename.H3rbsnet_antA.csv
-    python H3_rbs_check.py -i $directory/$name/$filename.H3siteB.csv -o $directory/$name/$filename.H3rbsnet_antB.csv
-    python H3_rbs_check.py -i $directory/$name/$filename.H3siteC.csv -o $directory/$name/$filename.H3rbsnet_antC.csv
-    python H3_rbs_check.py -i $directory/$name/$filename.H3siteD.csv -o $directory/$name/$filename.H3rbsnet_antD.csv
-    python H3_rbs_check.py -i $directory/$name/$filename.H3siteE.csv -o $directory/$name/$filename.H3rbsnet_antE.csv
+    python contact_diffs.py -i $name/$filename.H3static_contacts.csv -r $reference.H3static_contacts.csv -o $name/$filename.H3contactdiffs.static.csv
+    sleep 5
+    python get_contact_flare.py --input $name/$filename.H3static_contacts.csv --out $name/$filename.H3contact_flare.json    sleep 5
+    python H3_rbs_check.py -i $name/$filename.H3contactdiffs.static.csv -o $name/$filename.H3rbs.csv
+    sleep 5
+    python sin_check.py -i $name/$filename.H3contactdiffs.static.csv -o $name/$filename.H3_SIN.csv
+    python siteA_check.py -i $name/$filename.H3contactdiffs.static.csv -o $name/$filename.H3siteA.csv
+    python siteB_check.py -i $name/$filename.H3contactdiffs.static.csv -o $name/$filename.H3siteB.csv
+    python siteC_check.py -i $name/$filename.H3contactdiffs.static.csv -o $name/$filename.H3siteC.csv
+    python siteD_check.py -i $name/$filename.H3contactdiffs.static.csv -o $name/$filename.H3siteD.csv
+    python siteE_check.py -i $name/$filename.H3contactdiffs.static.csv -o $name/$filename.H3siteE.csv
+    python H3_rbs_check.py -i $name/$filename.H3siteA.csv -o $name/$filename.H3rbsnet_antA.csv
+    python H3_rbs_check.py -i $name/$filename.H3siteB.csv -o $name/$filename.H3rbsnet_antB.csv
+    python H3_rbs_check.py -i $name/$filename.H3siteC.csv -o $name/$filename.H3rbsnet_antC.csv
+    python H3_rbs_check.py -i $name/$filename.H3siteD.csv -o $name/$filename.H3rbsnet_antD.csv
+    python H3_rbs_check.py -i $name/$filename.H3siteE.csv -o $name/$filename.H3rbsnet_antE.csv
     #sed -i "s/:/\,/g" $filename.contactdiffs.static.csv
-    sed -i "s/\"//g" $directory/$name/$filename.H3contactdiffs.static.csv
-    tail -n +2 $directory/$name/$filename.H3contactdiffs.static.csv
-    sed '1i zero,bond_type,chain1,chain2' $directory/$name/$filename.H3contactdiffs.static.csv
-    sed -i "s/\"//g" $directory/$name/$filename.H3rbs.csv
-    tail -n +2 $directory/$name/$filename.H3rbs.csv
-    sed '1i zero,bond_type,chain1,chain2' $directory/$name/$filename.H3rbs.csv
-    sed -i "s/\"//g" $directory/$name/$filename.H3siteA.csv
-    tail -n +2 $directory/$name/$filename.H3siteA.csv
-    sed '1i zero,bond_type,chain1,chain2' $directory/$name/$filename.H3siteA.csv
-    sed -i "s/\"//g" $directory/$name/$filename.H3siteB.csv
-    tail -n +2 $directory/$name/$filename.H3siteB.csv
-    sed '1i zero,bond_type,chain1,chain2' $directory/$name/$filename.H3siteB.csv
-    sed -i "s/\"//g" $directory/$name/$filename.H3siteC.csv
-    tail -n +2 $directory/$name/$filename.H3siteC.csv
-    sed '1i zero,bond_type,chain1,chain2' $directory/$name/$filename.H3siteC.csv
-    sed -i "s/\"//g" $directory/$name/$filename.H3siteD.csv
-    tail -n +2 $directory/$name/$filename.H3siteD.csv
-    sed '1i zero,bond_type,chain1,chain2' $directory/$name/$filename.H3siteD.csv
-    sed -i "s/\"//g" $directory/$name/$filename.H3siteE.csv
-    tail -n +2 $directory/$name/$filename.H3siteE.csv
-    sed '1i zero,bond_type,chain1,chain2' $directory/$name/$filename.H3SsiteE.csv
+    sed -i "s/\"//g" $name/$filename.H3contactdiffs.static.csv
+    tail -n +2 $name/$filename.H3contactdiffs.static.csv
+    sed '1i zero,bond_type,chain1,chain2' $name/$filename.H3contactdiffs.static.csv
+    sed -i "s/\"//g" $name/$filename.H3rbs.csv
+    tail -n +2 $name/$filename.H3rbs.csv
+    sed '1i zero,bond_type,chain1,chain2' $name/$filename.H3rbs.csv
+    sed -i "s/\"//g" $name/$filename.H3siteA.csv
+    tail -n +2 $name/$filename.H3siteA.csv
+    sed '1i zero,bond_type,chain1,chain2' $name/$filename.H3siteA.csv
+    sed -i "s/\"//g" $name/$filename.H3siteB.csv
+    tail -n +2 $name/$filename.H3siteB.csv
+    sed '1i zero,bond_type,chain1,chain2' $name/$filename.H3siteB.csv
+    sed -i "s/\"//g" $name/$filename.H3siteC.csv
+    tail -n +2 $name/$filename.H3siteC.csv
+    sed '1i zero,bond_type,chain1,chain2' $name/$filename.H3siteC.csv
+    sed -i "s/\"//g" $name/$filename.H3siteD.csv
+    tail -n +2 $name/$filename.H3siteD.csv
+    sed '1i zero,bond_type,chain1,chain2' $name/$filename.H3siteD.csv
+    sed -i "s/\"//g" $name/$filename.H3siteE.csv
+    tail -n +2 $name/$filename.H3siteE.csv
+    sed '1i zero,bond_type,chain1,chain2' $name/$filename.H3siteE.csv
 done
-#cp count_contacts_compile.sh $directory/$name
-#bash $directory/$name/count_contacts_compile.sh -d $directory/$name -n $filename"_1.txt"
+cp /scicomp/home-pure/qxa4/BCP_V2/count_contacts_compile.sh $directory/$name/count_contacts_compile.sh
+bash $directory/$name/count_contacts_compile.sh -d $directory/$name -n $filename"_contacts_compile.txt"
 #do
 #    filename=$(basename "$file")
 #    python run_sasa.py -r $reference -p $filename -d $directory/$name/$filename.sasa.csv
@@ -111,52 +113,52 @@ for file in $directory/*.pdb
 do
     filename=$(basename $file);
     if [ ! -f  $filename.mae ] ; then
-        run pdbconvert -ipdb $filename -omae $directory/$name/$filename.mae
-        while ! [ -f $directory/$name/$filename.mae ]
+        run pdbconvert -ipdb $filename -omae $name/$filename.mae
+        while ! [ -f $name/$filename.mae ]
         do
             sleep 10
         done
-        run utilities/prepwizard $directory/$name/$filename.mae $directory/$name/$filename.mae
+        run utilities/prepwizard $name/$filename.mae $name/$filename.mae
     fi
 done
 #Generates a control file for running grid file for Glide
-while ! [ -f $directory/$name/$filename.mae ]
-    do
-        sleep 10
-    done
+#while ! [ -f $name/$filename.mae ]
+#    do
+#        sleep 10
+#    done
 
-for file in $directory/$name/prep*.mae
+for file in $name/prep*.mae
 do
     filename=$(basename "$file");
     gridtable="FORCEFIELD   OPLS_2005\nGRID_CENTER_ASL residue.num 153\nGRIDFILE    $directory/$name/"$filename"grid.zip\nINNERBOX   10, 10, 10\nOUTERBOX   30, 30, 30\nRECEP_FILE   "$file""
-    echo -e $gridtable > $directory/$name/$filename"grid.inp"
+    echo -e $gridtable > $name/$filename"grid.inp"
 #       bash run_grid_gen.sh -g $directory/$name
 done
 
 #Generate a control file for running Glide sialic acid docks from grid file
-for file in $directory/$name/prep*.mae
+for file in $name/prep*.mae
 do
     filename=$(basename "$file");
     #docktable="FORCEFIELD   OPLS_2005\nGRIDFILE   "$filename".mae_grid.zip\nLIGANDFILE   /scicomp/home-pure/qxa4/BCP_V2/ligprep_3-sialyl-out.maegz\nNREPORT   2\nPOSTDOCK_XP_DELE   0.5\nPRECISION   XP\nWRITE_XP_DESC   True\nWRITE_RES_INTERACTION   True\nWRITE_CSV       True"
-    docktable="FORCEFIELD   OPLS_2005\nGRIDFILE   $directory/$name/"$filename"_grid.zip\nLIGANDFILE   /scicomp/home-pure/qxa4/BCP_V2/ligprep_6-sialyl-out.maegz\nNREPORT   2\nPOSTDOCK_XP_DELE   0.5\nPRECISION   XP\nWRITE_XP_DESC   True\nWRITE_RES_INTERACTION   True\nWRITE_CSV       True"
-    echo -e $docktable > $directory/$name/$filename.dock.inp
+    docktable="FORCEFIELD   OPLS_2005\nGRIDFILE   $directory/$name/"$filename"grid.zip\nLIGANDFILE   /scicomp/home-pure/qxa4/BCP_V2/ligprep_6-sialyl-out.maegz\nNREPORT   2\nPOSTDOCK_XP_DELE   0.5\nPRECISION   XP\nWRITE_XP_DESC   True\nWRITE_RES_INTERACTION   True\nWRITE_CSV       True"
+    echo -e $docktable > $name/$filename.dock.inp
 #    bash run_ligdock.sh -l $directory
 done
 
 #Runs the sialic acid docking from the control file
-for file in $directory/$name/prep*.pdb.maegrid.inp
+for file in $name/prep*.pdb.maegrid.inp
 do
     filename=$(basename "$file");
-    run glide $directory/$name/$filename -OVERWRITE -HOST localhost -TMPLAUNCHDIR -LOCAL
-    while ! [ -f $directory/$name/$filename_grid.zip ]
+    run glide $name/$filename -OVERWRITE -HOST localhost -TMPLAUNCHDIR -LOCAL
+    while ! [ -f $name/$filename_grid.zip ]
     do
         sleep 20
     done
 done
-for file in $directory/$name/prep*.pdb.mae.dock.inp
+for file in $name/prep*.pdb.mae.dock.inp
 do
     filename=$(basename "$file");
-    run glide $directory/$name/$filename -OVERWRITE -HOST localhost -TMPLAUNCHDIR -LOCAL
+    run glide $name/$filename -OVERWRITE -HOST localhost -TMPLAUNCHDIR -LOCAL
 done
 #fi
 #Generate a control file for antibody docks
